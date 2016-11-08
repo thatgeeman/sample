1f887925aa0cf5ba3e02487db18929518a5dc8eb
thatgeeman <thatgeeman@gmail.com>
2016-11-09 00:29:11 +0100

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 34;
+    int y = 52;
+    
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+    
+    // swapping...
+    int temp = x;
+    x = y;
+    y = temp;
+    
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
